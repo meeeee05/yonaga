244b6812ce0cea42c664085d82c690c18230ce60
　🐺 芽🐺 <me@noMacBook-Air.local>
2022-10-09 23:20:05 +0900

    35

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+
 
 def frontpage(request):
-    return render(request,"blog/frontpage.html")
+    posts=Post.objects.all()
+
+    return render(request,"blog/frontpage.html",{"posts":posts})
