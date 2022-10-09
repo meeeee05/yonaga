c9e7c26fbcb5dee72341791cb68a01893c43f8bc
　🐺 芽🐺 <me@noMacBook-Air.local>
2022-10-09 18:45:57 +0900

    first commit

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    intro = models.TextField()
+    body = models.TextField()
+    posted_date=models.DateTimeField(auto_now_add=True)
+    
---
+++ microblog/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from blog.views import frontpage 
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("",frontpage)
+]
